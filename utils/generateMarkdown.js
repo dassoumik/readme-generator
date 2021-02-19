f078488a923e5c13f380559081cf0fa95a9530ab
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-02-19 12:28:00 -0500

    code adjustments in the markdown module

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 let badge;
 let licenseLink;
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
 function renderLicenseBadge(license) {
     switch (license) {
       case "MIT":
@@ -21,14 +21,11 @@ function renderLicenseBadge(license) {
         
       }
     return badge;
-  //https://img.shields.io/badge/license-MIT-yellowgreen
-  //https://img.shields.io/badge/license-Apache-yellowgreen
-  // https://img.shields.io/badge/license-ISC-yellowgreen
-  //https://img.shields.io/badge/license-GNU%20GPLv2-yellowgreen
+  
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function that returns the license link
+// If there is no license, returns an empty string
 function renderLicenseLink(license) { switch (license) {
   case "MIT":
     licenseLink = "https://choosealicense.com/licenses/mit/";
@@ -46,20 +43,12 @@ function renderLicenseLink(license) { switch (license) {
     break;
   }
 return licenseLink;}
-//https://choosealicense.com/licenses/mit/
-//https://choosealicense.com/licenses/apache-2.0/
-//https://choosealicense.com/licenses/isc/
-//https://choosealicense.com/licenses/gpl-2.0/
 
-
-
-
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function that returns the license section of README
+// If there is no license, returns an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   const badge = renderLicenseBadge(data.license);
   const licenseLink = renderLicenseLink(data.license);
@@ -95,13 +84,6 @@ function generateMarkdown(data) {
   ## Questions
   Please connnect with me at ${data.email} <br>
   ![](./utils/GitHub-Mark/PNG/GitHub-Mark-32px.png) [${data.yourname}](https://github.com/${data.gituserid})
-
-
-
-
-
-
-  
 
 `;
 }
